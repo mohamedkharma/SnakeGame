0249b466fb5b513c684d2308fc23f8d513beaae2
Mohamed Kharma <77759231+mohamedkharma@users.noreply.github.com>
2022-07-18 08:28:52 -0500

    Add files via upload

---
+++ Snake/src/GameFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);	// set the frame of the game to the middle of the screen
+	}
+}
